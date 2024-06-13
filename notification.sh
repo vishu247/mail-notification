7b571f300269763eac1f09f4e9f55b14e2140161
vishu247 <156403229+vishu247@users.noreply.github.com>
2024-06-14 00:41:20 +0530

    Update notification.sh

--- notification.sh
+++ notification.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$SECRET_KEY_ID
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws configure set region "$REGION"
+aws configure set region "$AWS_REGION"
 
 # Get instance information
 instance_info=$(aws ec2 describe-instances --region ${AWS_REGION} --query 'Reservations[*].Instances[*].[InstanceId,State.Name,Tags[?Key==`owner`].Value]' --output json)
@@ -28,41 +28,41 @@ for ((i=0; i<${#instance_ids[@]}; i++)); do
     if [ "$instance_state" == "running" ]; then
         # HTML content for the email
         HTML_CONTENT=$(cat <<EOF
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <style>
-                .button {
-                    display: inline-block;
-                    padding: 10px 20px;
-                    font-size: 14px;
-                    cursor: pointer;
-                    text-align: center;
-                    text-decoration: none;
-                    outline: none;
-                    color: #fff;
-                    background-color: #FFD700; /* Yellow color */
-                    border: none;
-                    border-radius: 15px;
-                    box-shadow: 0 9px #999;
-                }
-                .button:hover {background-color: #FFCC00} /* Darker yellow on hover */
-                .button:active {
-                    background-color: #FFCC00;
-                    box-shadow: 0 5px #666;
-                    transform: translateY(4px);
-                }
-            </style>
-        </head>
-        <body>
-            <h2>Your virtual machine in AWS QA Labs will automatically shut down</h2>
-            <p>Virtual machine with Instance ID <strong>$instance_id</strong> is scheduled to shut down in 2 hours. You may skip this instance shutdown by clicking on the below button:</p>
-            <p>
-                <a href="$LambdaFunctionUrl?instanceId=$instance_id" class="button">Skip Instance Shutdown ></a>
-            </p>
-            <p>Note that if you skip the shutdown, a new notification will be sent 2 hours prior to the new shutdown time.</p>
-        </body>
-        </html>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 14px;
+            cursor: pointer;
+            text-align: center;
+            text-decoration: none;
+            outline: none;
+            color: #fff;
+            background-color: #FFD700; /* Yellow color */
+            border: none;
+            border-radius: 15px;
+            box-shadow: 0 9px #999;
+        }
+        .button:hover {background-color: #FFCC00} /* Darker yellow on hover */
+        .button:active {
+            background-color: #FFCC00;
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
+        }
+    </style>
+</head>
+<body>
+    <h2>Your virtual machine in AWS QA Labs will automatically shut down</h2>
+    <p>Virtual machine with Instance ID <strong>$instance_id</strong> is scheduled to shut down in 2 hours. You may skip this instance shutdown by clicking on the below button:</p>
+    <p>
+        <a href="$LambdaFunctionUrl?instanceId=$instance_id" class="button">Skip Instance Shutdown ></a>
+    </p>
+    <p>Note that if you skip the shutdown, a new notification will be sent 2 hours prior to the new shutdown time.</p>
+</body>
+</html>
 EOF
         )
 
@@ -78,7 +78,7 @@ EOF
             vm_name=$(echo "$owner_email" | tr "@" "-")
             aws ses send-email \
             --region "$AWS_REGION_MAIL" \
-            --from "$Sender" \
+            --from "$SENDER" \
             --destination "ToAddresses=$owner_email" \
             --message "Subject={Data=Alert—Auto-shutdown of virtual machine $vm_name in 2 hours,Charset=utf-8},Body={Html={Data='$HTML_CONTENT',Charset=utf-8}}"
         else
@@ -92,5 +92,3 @@ EOF
         echo "Instance $instance_id is not in a running state. Skipping email notification."
     fi
 done
-
-
