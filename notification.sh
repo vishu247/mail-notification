81604a134926c46a39ed392ef96b9f544f122b6a
vishu247 <156403229+vishu247@users.noreply.github.com>
2024-06-14 12:15:34 +0530

    Update notification.sh

--- notification.sh
+++ notification.sh
@@ -2,6 +2,7 @@
 
 export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$SECRET_KEY_ID
+export AWS_REGION=$AWS_REGION
 aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 aws configure set region "$AWS_REGION"
